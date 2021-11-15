a8a30a47b10c334d99af01a0ca8d52cc03f18d47
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-11-15 14:00:09 +0100

    Refactor: extract compute diff from run test

--- run_tests.sh
+++ run_tests.sh
@@ -7,27 +7,29 @@ reset=$(tput sgr0)
 echo_with_color() {
     echo -n "$1"
     args=$#
-    for (( i=2; i<=$args; i+=1 ))
+    for (( i = 2; i <= args; i += 1 ))
     do
-        echo ${!i}
+        echo "${!i}"
     done
     echo -n "${reset}"
 }
 
-compute_diff(){
-    
+compute_diff() {
     ./bin/fizzbuzz.sh "$1" > test_output.txt
-    # grab output of diff -> if 
-    DIFF=$(diff -y --suppress-common-lines ./test/fizzbuzz_"$1"_expected_output.txt test_output.txt) 
-    if [ "$DIFF" != "" ] 
+    diff -y --suppress-common-lines ./test/fizzbuzz_"$1"_expected_output.txt test_output.txt
+}
+
+run_test(){
+    diff=$(compute_diff "$1")
+    if [ "$diff" != "" ] 
     then
-        echo_with_color "${red}" test_"$1" "expected                                                      | actual" "${DIFF}" 
+        echo_with_color "${red}" test_"$1" "expected                                                      | actual" "${diff}" 
     else 
         echo_with_color "${green}" test_"$1"
     fi
 }
 
-compute_diff 1
-compute_diff 2
-compute_diff 3
-compute_diff 5
+run_test 1
+run_test 2
+run_test 3
+run_test 5
