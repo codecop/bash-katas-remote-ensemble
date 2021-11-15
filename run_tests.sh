07a73e92d7f2cc5931c2d822e6b3be47081ae503
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-11-15 13:27:13 +0100

    Test turns red if fails.

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
 compute_diff(){
     echo test_"$1"
     ./bin/fizzbuzz.sh "$1" > test_output.txt
-    diff -y --suppress-common-lines test_output.txt ./test/fizzbuzz_"$1"_expected_output.txt
+    # grab output of diff -> if 
+    DIFF=$(diff -y --suppress-common-lines ./test/fizzbuzz_"$1"_expected_output.txt test_output.txt) 
+    if [ "$DIFF" != "" ] 
+    then
+        echo "${red}"
+        echo "expected                                                      | actual"
+        echo "${DIFF}"
+        echo "${reset}"
+    fi
 }
 
 compute_diff 1
