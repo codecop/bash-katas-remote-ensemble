4b8dd60e7703ac952ec47ebd62566e6a8b11b95c
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-11-29 13:49:58 +0100

    [WIP] concatenate result

--- bin/fizzbuzz.sh
+++ bin/fizzbuzz.sh
@@ -2,16 +2,18 @@
 
 for i in $(seq 1 "$1"); 
 do 
-    output_value="$i"
+    output_value=""
     if [ $(( $i % 3)) == 0 ] && [ $(( $i % 5)) == 0 ]
     then
         output_value="fizzbuzz"
     elif (( "$i" % 3 == 0 ))
     then
-        output_value="fizz"
+        output_value=${output_value}"fizz"
     elif (( "$i" % 5 == 0 ))
     then
-        output_value="buzz"
+        output_value=${output_value}"buzz"
+    else 
+        output_value="$i"
     fi
     echo ${output_value}
 done
