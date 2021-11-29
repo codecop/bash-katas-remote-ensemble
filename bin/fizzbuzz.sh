fc69074d025a0b681b5093da7d8b6db388f11122
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-11-29 14:08:25 +0100

    Refactor: extract function get_word

--- bin/fizzbuzz.sh
+++ bin/fizzbuzz.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+get_word() {
+    number=$1
+    modulus=$2
+    string=$3
+    word=$4
+    if (( "$number" % "$modulus" == 0 ))
+    then
+        echo "${string}${word}"
+    else
+        echo "${string}"
+    fi
+}
+
 for i in $(seq 1 "$1"); 
 do 
     output_value=""
-    if (( "$i" % 3 == 0 ))
-    then
-        output_value=${output_value}"fizz"
-    fi
-    if (( "$i" % 5 == 0 ))
-    then
-        output_value=${output_value}"buzz"
-    fi 
+    output_value=$(get_word "$i" 3 "$output_value" "fizz")
+    output_value=$(get_word "$i" 5 "$output_value" "buzz")
     if [ "$output_value" == "" ] 
     then
         output_value="$i"
     fi
-    echo ${output_value}
+    echo "${output_value}"
 done
